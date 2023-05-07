d8a81b5e14141a65f2b68e7332ca8e99d2e8cb76
Olaf Liebe <olaf.liebe@ollb.de>
2023-05-07 02:33:39 +0200

    More reasonable function naming. Some error handling.

--- lua/dark-mode.lua
+++ lua/dark-mode.lua
@@ -1,14 +1,32 @@
 local M = {}
 
-function M:detect()
+function M:is_light()
 	local cmd = 'reg query HKCU\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize /v AppsUseLightTheme'
 
-	local file = assert(io.popen(cmd, 'r'))
-	local output = assert(file:read('*all'))
+	local file = io.popen(cmd, 'r')
+
+	if file == nil then
+		return true -- Assume light theme on error.
+	end
+
+	local output = file:read('*all')
 	file:close()
 
-	local result = output:match('AppsUseLightTheme%s+REG_DWORD%s+0x(%d+)')
-	print(result)
+	if output == nil then
+		return true
+	end
+
+	local value = output:match('AppsUseLightTheme%s+REG_DWORD%s+0x(%d+)')
+
+	if value == nil then
+		return true
+	end
+
+	return value == '1'
+end
+
+function M:is_dark()
+	return not self:is_light()
 end
 
 return M
